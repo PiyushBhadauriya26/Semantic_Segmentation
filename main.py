85a7e42ac58ceeba79be2b0f25e71c1e4a09d7ae
Piyush <pibhadau@cisco.com>
2025-01-21 20:43:52 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from segment_anything import SamAutomaticMaskGenerator, sam_model_registry
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import cv2
+
+def get_masks(image,model_checkpoint="model_checkpoint/sam_vit_h_4b8939.pth"):
+    sam = sam_model_registry["default"](checkpoint=model_checkpoint)
+    sam.to(device="cuda")
+    mask_generator = SamAutomaticMaskGenerator(sam)
+    return mask_generator.generate(image)
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+if __name__ == '__main__':
+    image_path = 'data/test1.png'
+    image = cv2.imread(image_path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    masks = get_masks(image)
+    print(len(masks))
+    print(masks[0].keys())
+    plt.figure(figsize=(20, 20))
+    plt.imshow(image)
+    show_anns(masks)
+    plt.axis('off')
+    plt.savefig('data/segmented_img1.png')
+    plt.show()
+
+
